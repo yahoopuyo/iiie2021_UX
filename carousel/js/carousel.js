e38f66ac958a2ebfaa9537968a2cee98a3f4b4c5
yahoopuyo <yahoopuyo@gmail.com>
2021-06-26 11:16:34 +0900

    updated carousel button and page transition

--- carousel/js/carousel.js
+++ carousel/js/carousel.js
@@ -1,18 +1,14 @@
-  // $("#project").click(function() {
-  //   window.location.href = "../../project1/index.html";
-  // }
-
-
 window.addEventListener('load', () => {
 	var
-		carousels = document.querySelectorAll('.carousel')
-	;
+		carousels = document.querySelectorAll('.carousel');
 
 	for (var i = 0; i < carousels.length; i++) {
 		carousel(carousels[i]);
 	}
 });
 
+
+// カルーセルの処理
 function carousel(root) {
 	var
 		figure = root.querySelector('figure'),
@@ -25,7 +21,8 @@ function carousel(root) {
 		bfc = 'bfc' in root.dataset,
 
 		theta =  2 * Math.PI / n,
-		currImage = 0
+		currImage = 0,
+    able2click = true;
 	;
 
 	setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
@@ -56,27 +53,53 @@ function carousel(root) {
 	}
 
 	function setupNavigation() {
-		nav.addEventListener('click', onClick, true);
+		nav.addEventListener('click', onClick_nav, true);
+    figure.addEventListener('click', onClick_fig, true);
 
-		function onClick(e) {
+    figure.addEventListener('mouseover', function(e){
+      var t = e.target;
+      if (t.classList.contains('project')) if(t.classList[1] == currImage+1){
+        t.style.cursor = "pointer";
+      }
+    },true);
+
+		function onClick_nav(e) {
+      // alert("nav clicked")
 			e.stopPropagation();
 
 			var t = e.target;
+      // if (t.classList.contains('transparent')){
+      //   joinProjectPage();
+      // }
 			if (t.tagName.toUpperCase() != 'BUTTON')
 				return;
 
 			if (t.classList.contains('next')) {
 				currImage++;
+        rotateCarousel(currImage);
 			}
 			else if (t.classList.contains('prev')){
 				currImage--;
+        rotateCarousel(currImage);
 			}
       else {
-        window.location.href = "../project1/index.html";
+        joinProjectPage()
       }
-
-			rotateCarousel(currImage);
 		}
+
+    function onClick_fig(e) {
+      e.stopPropagation();
+      var t = e.target;
+      if (t.classList.contains('project')){
+        if (t.classList[1] == currImage+1) joinProjectPage();
+      }
+    }
+
+    function joinProjectPage() {
+      // joinの時の遷移先の処理
+      var project_id = (currImage % 2) + 1;
+      window.location.href = `../project${project_id}/index.html`;
+    }
 
 	}
 
@@ -86,6 +109,10 @@ function carousel(root) {
 
 }
 
+
+// $("#transparent").click(function() {
+//   window.location.href = "../../project1/index.html";
+// }
 
 
 
